c997ccc233fd59009b1c98e6ffde971fa0454a33
Anciaux Guillaume <guillaume.anciaux@epfl.ch>
2011-12-01 12:31:28 +0000

    revert separation for boost: TEMPORARY

--- src/common/aka_common_inline_impl.cc
+++ src/common/aka_common_inline_impl.cc
@@ -29,27 +29,3 @@
  *
  */
 
-/* -------------------------------------------------------------------------- */
-// BOOST PART: TOUCH ONLY IF YOU KNOW WHAT YOU ARE DOING
-#include <boost/preprocessor.hpp>
-
-#define AKANTU_BOOST_CASE_MACRO(r,macro,type)	\
-  case type : { macro(type); break;}
-
-#define AKANTU_BOOST_ELEMENT_SWITCH(macro);				\
-  do {									\
-    switch(type) {							\
-      BOOST_PP_SEQ_FOR_EACH(AKANTU_BOOST_CASE_MACRO,macro,AKANTU_ELEMENT_TYPE) \
-    case _not_defined:							\
-    case _max_element_type:  {						\
-      AKANTU_DEBUG_ERROR("Wrong type : " << type);			\
-      break;								\
-    }									\
-    }									\
-  } while(0)
-
-#define AKANTU_BOOST_LIST_MACRO(r,macro,type)	\
-  macro(type)
-
-#define AKANTU_BOOST_ELEMENT_LIST(macro)				\
-  BOOST_PP_SEQ_FOR_EACH(AKANTU_BOOST_LIST_MACRO,macro,AKANTU_ELEMENT_TYPE)
