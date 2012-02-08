0c488144495fcef0825e465262b9325562eafde6
Yastrebov Vladislav <vladislav.yastrebov@epfl.ch>
2012-02-08 15:53:31 +0000

    Basic visco-elastic material

---
+++ src/model/solid_mechanics/materials/material_viscoelastic.cc
@@ -0,0 +1,224 @@
+/**
+ * @file   material_viscoelastic.hh
+ * @author Vlad Yastrebov <vladislav.yastrebov@epfl.ch> 
+ * @date   Thu Feb 7 2012 
+ *
+ * @brief  Material Visco-elastic
+ *
+ * @section LICENSE
+ *
+ * Copyright (©) 2010-2011 EPFL (Ecole Polytechnique Fédérale de Lausanne)
+ * Laboratory (LSMS - Laboratoire de Simulation en Mécanique des Solides)
+ *
+ * Akantu is free  software: you can redistribute it and/or  modify it under the
+ * terms  of the  GNU Lesser  General Public  License as  published by  the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Akantu is  distributed in the  hope that it  will be useful, but  WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A  PARTICULAR PURPOSE. See  the GNU  Lesser General  Public License  for more
+ * details.
+ *
+ * You should  have received  a copy  of the GNU  Lesser General  Public License
+ * along with Akantu. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/* -------------------------------------------------------------------------- */
+#include "material_viscoelastic.hh"
+#include "solid_mechanics_model.hh"
+
+__BEGIN_AKANTU__
+
+/* -------------------------------------------------------------------------- */
+MaterialViscoElastic::MaterialViscoElastic(Model & model, const ID & id)  :
+  Material(model, id), MaterialElastic(model, id),
+  stress_viscosity("stress_viscosity", id),
+  stress_elastic("stress_elastic", id) {
+  AKANTU_DEBUG_IN();
+
+  memset(stress_deviator, 0, 3 * 3 * sizeof(Real));
+  eta = 1.;
+  Ev  = 1.;
+  h   = 0.;
+  UInt spatial_dimension = this->model->getSpatialDimension();
+  UInt stress_size = spatial_dimension * spatial_dimension;
+
+  initInternalVector(this->stress_viscosity, stress_size);
+  initInternalVector(this->stress_elastic  , stress_size);
+
+
+  AKANTU_DEBUG_OUT();
+}
+
+/* -------------------------------------------------------------------------- */
+void MaterialViscoElastic::initMaterial() {
+  AKANTU_DEBUG_IN();
+  MaterialElastic::initMaterial();
+
+  resizeInternalVector(this->stress_viscosity);
+  resizeInternalVector(this->stress_elastic  );
+
+  AKANTU_DEBUG_OUT();
+}
+
+/* -------------------------------------------------------------------------- */
+void MaterialViscoElastic::computeStress(ElementType el_type, GhostType ghost_type) {
+  AKANTU_DEBUG_IN();
+
+  Real Theta, tau = eta/Ev, K = E / ( 3 * (1 - 2*nu) );
+  Real e[3*3], s[3*3];
+  Real sigma[3*3];
+
+  MATERIAL_STRESS_QUADRATURE_POINT_LOOP_BEGIN;
+  memset(e, 0, 3 * 3 * sizeof(Real));
+  memset(s, 0, 3 * 3 * sizeof(Real));
+  memset(sigma, 0, 3 * 3 * sizeof(Real));
+
+  /// Compute the first invariant of strain
+  Theta = 0;
+  for (UInt i = 0; i < spatial_dimension; ++i)
+    Theta += strain_val[spatial_dimension * i + i];
+
+  /// Compute the deviator of strain
+  for (UInt i = 0; i < spatial_dimension; ++i)
+    for (UInt j = 0; j < spatial_dimension; ++j)
+      if (i == j)
+        e[3*i + j] = strain_val[spatial_dimension * i + j] - 1/double(spatial_dimension)*Theta;
+      else
+        e[3*i + j] = strain_val[spatial_dimension * i + j];
+
+  /// Compute the deviator of stress
+  for (UInt i = 0; i < spatial_dimension; ++i)
+    for (UInt j = 0; j < spatial_dimension; ++j)
+       s[3*i + j] = E / (1 + nu) * e[3*i + j];
+ 
+  /// Update the internal variable
+  for (UInt i = 0; i < spatial_dimension; ++i)
+    for (UInt j = 0; j < spatial_dimension; ++j)
+      h[3*i + j] = exp( -dt/tau ) * h[3*i+j] + exp( -0.5*dt/tau ) * ( stress_deviator[3*i + j] + s[3*i + j] );
+
+  for (UInt i = 0; i < spatial_dimension; ++i)
+    for (UInt j = 0; j < spatial_dimension; ++j)  
+      if (i == j)
+        sigma[3*i + j] = K * Theta + ( E * s[3*i + j] + Ev * h[3*i + j] ) / ( Ev + E );
+      else
+        sigma[3*i + j] = ( E * s[3*i + j] + Ev * h[3*i + j] ) / ( Ev + E );
+  
+  /// Save the deviator of stress
+  for (UInt i = 0; i < spatial_dimension; ++i)
+    for (UInt j = 0; j < spatial_dimension; ++j)
+       stress_deviator[3*i + j] = s[3*i + j];
+
+//  computeStress(F, sigma);
+
+  for (UInt i = 0; i < spatial_dimension; ++i)
+    for (UInt j = 0; j < spatial_dimension; ++j)
+      stress_val[spatial_dimension*i + j] = sigma[3 * i + j];
+
+  MATERIAL_STRESS_QUADRATURE_POINT_LOOP_END;
+
+/*
+  UInt spatial_dimension = model->getSpatialDimension();
+
+  Vector<UInt> & elem_filter = element_filter  (el_type, ghost_type);
+  Vector<Real> & stress_visc = stress_viscosity(el_type, ghost_type);
+  Vector<Real> & stress_el   = stress_elastic  (el_type, ghost_type);
+  //mu       = E / (2 * (1 + nu));
+
+  MaterialElastic::computeStress(el_type, ghost_type);
+
+  Vector<Real> & velocity = model->getVelocity();
+  UInt nb_elem = element_filter(el_type, ghost_type).getSize();
+  UInt nb_quad_points_per_elem =
+    model->getFEM().getNbQuadraturePoints(el_type, ghost_type);
+  UInt nb_quad_points = nb_quad_points_per_elem * nb_elem;
+
+  Vector<Real> strain_rate(nb_quad_points,
+			   spatial_dimension * spatial_dimension,
+			   "strain_rate");
+
+  model->getFEM().gradientOnQuadraturePoints(velocity, strain_rate,
+   					     spatial_dimension,
+   					     el_type, ghost_type, &elem_filter);
+
+  Real F[3*3];
+  Real sigma[3*3];
+  Real * strain_rate_val = strain_rate.storage();
+  Real * stress_visc_val = stress_visc.storage();
+  Real * stress_el_val   = stress_el.storage();
+
+  MATERIAL_STRESS_QUADRATURE_POINT_LOOP_BEGIN;
+  memset(F, 0, 3 * 3 * sizeof(Real));
+
+  for (UInt i = 0; i < spatial_dimension; ++i)
+    for (UInt j = 0; j < spatial_dimension; ++j)
+      F[3*i + j] = strain_rate_val[spatial_dimension * i + j];
+
+  MaterialElastic::computeStress(F, sigma);
+
+  for (UInt i = 0; i < spatial_dimension; ++i)
+    for (UInt j = 0; j < spatial_dimension; ++j) {
+//      stress_visc_val[spatial_dimension * i + j]  = alpha * sigma[3 * i + j];
+      stress_el_val  [spatial_dimension * i + j]  = stress_val[spatial_dimension*i + j];
+//      stress_val     [spatial_dimension * i + j] += stress_visc_val[spatial_dimension*i + j];
+    }
+
+  strain_rate_val += spatial_dimension * spatial_dimension;
+//  stress_visc_val += spatial_dimension * spatial_dimension;
+  stress_el_val   += spatial_dimension * spatial_dimension;
+
+  MATERIAL_STRESS_QUADRATURE_POINT_LOOP_END;
+*/
+  AKANTU_DEBUG_OUT();
+}
+
+/* -------------------------------------------------------------------------- */
+void MaterialViscoElastic::computePotentialEnergy(ElementType el_type, GhostType ghost_type) {
+  AKANTU_DEBUG_IN();
+/*
+  if(ghost_type != _not_ghost) return;
+
+  Vector<Real> & stress_el = stress_elastic(el_type, ghost_type);
+  Real * stress_el_val = stress_el.storage();
+
+  Real * epot = potential_energy(el_type, ghost_type).storage();
+
+  MATERIAL_STRESS_QUADRATURE_POINT_LOOP_BEGIN;
+
+  MaterialElastic::computePotentialEnergy(strain_val, stress_el_val, epot);
+  epot++;
+  stress_el_val += spatial_dimension*spatial_dimension;
+
+  MATERIAL_STRESS_QUADRATURE_POINT_LOOP_END;
+*/
+  AKANTU_DEBUG_OUT();
+}
+
+/* -------------------------------------------------------------------------- */
+bool MaterialViscoElastic::setParam(const std::string & key, const std::string & value,
+				      const ID & id) {
+  std::stringstream sstr(value);
+  if(key == "eta") { sstr >> eta; }
+  else if(key == "Ev") { sstr >> Ev; }
+  else { return MaterialElastic::setParam(key, value, id); }
+  return true;
+}
+
+
+/* -------------------------------------------------------------------------- */
+void MaterialViscoElastic::printself(std::ostream & stream, int indent) const {
+/*
+  std::string space;
+  for(Int i = 0; i < indent; i++, space += AKANTU_INDENT);
+
+  stream << space << "MaterialViscoElastic [" << std::endl;
+  MaterialElastic::printself(stream, indent + 1);
+  stream << space << " + artifical viscous ratio : " << alpha << std::endl;
+  stream << space << "]" << std::endl;
+*/
+}
+/* -------------------------------------------------------------------------- */
+
+__END_AKANTU__
---
+++ src/model/solid_mechanics/materials/material_viscoelastic.hh
@@ -0,0 +1,132 @@
+/**
+ * @file   material_viscoelastic.hh
+ * @author Vlad Yastrebov <vladislav.yastrebov@epfl.ch> 
+ * @date   Thu Feb 7 2012 
+ *
+ * @brief  Material Visco-elastic, based on Standard Solid rheological model, see [] J.C. Simo, T.J.R. Hughes, "Computational Inelasticity", Springer (1998), see Sections 10.2 and 10.3
+ *
+ * @section LICENSE
+ *
+ * Copyright (©) 2010-2011 EPFL (Ecole Polytechnique Fédérale de Lausanne)
+ * Laboratory (LSMS - Laboratoire de Simulation en Mécanique des Solides)
+ *
+ * Akantu is free  software: you can redistribute it and/or  modify it under the
+ * terms  of the  GNU Lesser  General Public  License as  published by  the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Akantu is  distributed in the  hope that it  will be useful, but  WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A  PARTICULAR PURPOSE. See  the GNU  Lesser General  Public License  for more
+ * details.
+ *
+ * You should  have received  a copy  of the GNU  Lesser General  Public License
+ * along with Akantu. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/* -------------------------------------------------------------------------- */
+#include "aka_common.hh"
+#include "material.hh"
+/* -------------------------------------------------------------------------- */
+
+#ifndef __AKANTU_MATERIAL_VISCOELASTIC_HH__
+#define __AKANTU_MATERIAL_VISCOELASTIC_HH__
+
+__BEGIN_AKANTU__
+
+/**
+ * Material viscoelastic (caughey condition) isotropic
+ *
+ * parameters in the material files :
+ *   - rho : density (default: 0)
+ *   - E   : Young's modulus (default: 0)
+ *   - nu  : Poisson's ratio (default: 1/2)
+ *   - Plane_Stress : if 0: plane strain, else: plane stress (default: 0)
+ *   - eta : viscosity
+ *   - Ev  : stiffness of the viscous element
+ *   - h   : internal variable of the integral history
+ */
+class MaterialViscoElastic : public MaterialElastic {
+  /* ------------------------------------------------------------------------ */
+  /* Constructors/Destructors                                                 */
+  /* ------------------------------------------------------------------------ */  
+public:
+
+  MaterialViscoElastic(Model & model, const ID & id = "");
+
+  virtual ~MaterialViscoElastic() {};
+
+  /* ------------------------------------------------------------------------ */
+  /* Methods                                                                  */
+  /* ------------------------------------------------------------------------ */
+public:
+
+  void initMaterial();
+
+  virtual bool setParam(const std::string & key, const std::string & value,
+			const ID & id);
+
+  /// constitutive law for all element of a type
+  void computeStress(ElementType el_type, GhostType ghost_type = _not_ghost);
+
+  /// compute the potential energy for all elements
+  virtual void computePotentialEnergy(ElementType el_type, GhostType ghost_type = _not_ghost);
+
+  /// function to print the containt of the class
+  virtual void printself(std::ostream & stream, int indent = 0) const;
+
+protected:
+  /// constitutive law for a given quadrature point
+  //__aka_inline__ void computeStress(Real * F, Real * sigma);
+
+  /* ------------------------------------------------------------------------ */
+  /* Accessors                                                                */
+  /* ------------------------------------------------------------------------ */
+public:
+  AKANTU_GET_MACRO_BY_ELEMENT_TYPE_CONST(StressViscosity, stress_viscosity, Real);
+  AKANTU_GET_MACRO_BY_ELEMENT_TYPE_CONST(StressElastic,   stress_elastic, Real);
+
+  AKANTU_GET_MACRO(Alpha, alpha, const Real&); //???
+  AKANTU_SET_MACRO(Alpha, alpha, Real &); //???
+
+  /* ------------------------------------------------------------------------ */
+  /* Class Members                                                            */
+  /* ------------------------------------------------------------------------ */
+private:
+
+  /// stress due to viscosity
+  ByElementTypeReal stress_viscosity;
+
+  /// stress due to elasticity
+  ByElementTypeReal stress_elastic;
+
+  /// viscosity, viscous elastic modulus
+  Real eta, Ev;
+
+  /// history of deviatoric stress
+  ByElementTypeReal stress_deviator[3*3];
+
+  /// Internal variable: strain history
+  ByElementTypeReal h;
+};
+
+/* -------------------------------------------------------------------------- */
+/* __aka_inline__ functions                                                           */
+/* -------------------------------------------------------------------------- */
+
+//#include "material_elastic_caughey_inline_impl.cc"
+
+/* -------------------------------------------------------------------------- */
+/// standard output stream operator
+/*
+inline std::ostream & operator <<(std::ostream & stream, const MaterialViscoElastic & _this)
+{
+  _this.printself(stream);
+  return stream;
+}
+*/
+__END_AKANTU__
+
+#endif /* __AKANTU_MATERIAL_VISCOELASTIC_HH__ */
+
