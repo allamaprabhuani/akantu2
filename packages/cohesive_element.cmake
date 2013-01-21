0f3f0a9bac83d9a3cb106df8e807bd71ca6f5b7a
Vocialta Marco <marco.vocialta@epfl.ch>
2013-01-21 12:11:14 +0000

    Inline file added to cohesive element package

--- packages/cohesive_element.cmake
+++ packages/cohesive_element.cmake
@@ -47,6 +47,7 @@ set(AKANTU_COHESIVE_ELEMENT_FILES
   model/solid_mechanics/materials/material_cohesive/constitutive_laws/material_cohesive_exponential_inline_impl.cc
 
   model/solid_mechanics/solid_mechanics_model_cohesive.cc
+  model/solid_mechanics/solid_mechanics_model_cohesive_inline_impl.cc
   model/solid_mechanics/materials/material_cohesive/material_cohesive.cc
   model/solid_mechanics/materials/material_cohesive/constitutive_laws/material_cohesive_linear.cc
   model/solid_mechanics/materials/material_cohesive/constitutive_laws/material_cohesive_bilinear.cc
