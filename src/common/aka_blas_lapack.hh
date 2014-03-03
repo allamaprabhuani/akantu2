54eaf570571734608c4a18559974b4afe771bb1e
Richart Nicolas <nicolas.richart@epfl.ch>
2014-03-03 17:01:25 +0000

    warning correction for intel

--- src/common/aka_blas_lapack.hh
+++ src/common/aka_blas_lapack.hh
@@ -71,34 +71,37 @@ extern "C" {
 #endif
 
 __BEGIN_AKANTU__
+
 #if defined(__INTEL_COMPILER)
 //#pragma warning ( disable : 383 )
-#elif defined(__GNUG__)
+#elif defined (__clang__) // test clang to be sure that when we test for gnu it is only gnu
+#elif (defined(__GNUC__) || defined(__GNUG__))
 #  define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #  if GCC_VERSION > 40600
 #    pragma GCC diagnostic push
 #  endif
 #  pragma GCC diagnostic ignored "-Wunused"
 #endif
+
 template<typename T>
 inline T aka_dot(int *n, T *x, int *incx, T *y, int *incy) {
-AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated BLAS in the compilation options!");
+  AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated BLAS in the compilation options!");
 }
 
 template<typename T>
 inline int aka_gemv(char *trans, int *m, int *n, T *
-                     alpha, T *a, int *lda, T *x, int *incx,
-                     T *beta, T *y, int *incy) {
-AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated BLAS in the compilation options!");
+		    alpha, T *a, int *lda, T *x, int *incx,
+		    T *beta, T *y, int *incy) {
+  AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated BLAS in the compilation options!");
 }
 
 template<typename T>
 inline int aka_gemm(char *transa, char *transb,
-                      int *m, int *n, int *k,
-                      T *alpha, T *a, int *lda,
-                      T *b, int *ldb,
-                      T *beta, T *c, int *ldc) {
-AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated BLAS in the compilation options!");
+		    int *m, int *n, int *k,
+		    T *alpha, T *a, int *lda,
+		    T *b, int *ldb,
+		    T *beta, T *c, int *ldc) {
+  AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated BLAS in the compilation options!");
 }
 
 #if defined(AKANTU_USE_BLAS)
@@ -109,8 +112,8 @@ inline double aka_dot<double>(int *n, double *x, int *incx, double *y, int *incy
 
 template<>
 inline int aka_gemv<double>(char *trans, int *m, int *n, double *
-			     alpha, double *a, int *lda, double *x, int *incx,
-			     double *beta, double *y, int *incy) {
+			    alpha, double *a, int *lda, double *x, int *incx,
+			    double *beta, double *y, int *incy) {
   return AKA_FC_GLOBAL(dgemv, DGEMV)(trans, m, n, alpha, a, lda, x, incx,
                                      beta, y, incy);
 }
@@ -135,8 +138,8 @@ inline float aka_dot<float>(int *n, float *x, int *incx, float *y, int *incy) {
 
 template<>
 inline int aka_gemv<float>(char *trans, int *m, int *n, float *
-			    alpha, float *a, int *lda, float *x, int *incx,
-			    float *beta, float *y, int *incy) {
+			   alpha, float *a, int *lda, float *x, int *incx,
+			   float *beta, float *y, int *incy) {
   return AKA_FC_GLOBAL(sgemv, SGEMV)(trans, m, n, alpha, a, lda, x, incx,
                                      beta, y, incy);
 }
@@ -184,7 +187,7 @@ extern "C" {
   /* ------------------------------------------------------------------------ */
   /* Simple general matrix                                                    */
   /* ------------------------------------------------------------------------ */
-    /// compute the eigenvalues/vectors
+  /// compute the eigenvalues/vectors
   void AKA_FC_GLOBAL(sgeev, SGEEV)(char* jobvl, char* jobvr, int* n, float* a,
                                    int* lda, float* wr, float* wi, float* vl, int* ldvl,
                                    float* vr, int* ldvr, float* work, int* lwork, int* info);
@@ -209,31 +212,35 @@ __BEGIN_AKANTU__
 
 template<typename T>
 inline void aka_geev(char* jobvl, char* jobvr, int* n, T* a,
-		       int* lda, T* wr, T* wi, T* vl, int* ldvl,
-		       T* vr, int* ldvr, T* work, int* lwork, int* info) {
-AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated LAPACK in the compilation options!");
+		     int* lda, T* wr, T* wi, T* vl, int* ldvl,
+		     T* vr, int* ldvr, T* work, int* lwork, int* info) {
+  AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated LAPACK in the compilation options!");
 }
 
 template<typename T>
 inline void aka_getrf(int* m, int *n,
-		       T* a, int* lda,
-		       int* ipiv, int* info) {
-AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated LAPACK in the compilation options!");
+		      T* a, int* lda,
+		      int* ipiv, int* info) {
+  AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated LAPACK in the compilation options!");
 }
 
 template<typename T>
 inline void aka_getri(int* n, T* a, int* lda,
-                       int* ipiv, T* work, int* lwork, int* info) {
-AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated LAPACK in the compilation options!");
+		      int* ipiv, T* work, int* lwork, int* info) {
+  AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated LAPACK in the compilation options!");
 }
 
 template<typename T>
 inline void aka_getrs(char *trans, int * n, int * nrhs,
-		       T * A, int * lda, int * ipiv,
-		       T * b, int * ldb, int * info) {
-AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated LAPACK in the compilation options!");
+		      T * A, int * lda, int * ipiv,
+		      T * b, int * ldb, int * info) {
+  AKANTU_DEBUG_ERROR(debug::demangle(typeid(T).name()) << "is not a type recognized, or you didn't activated LAPACK in the compilation options!");
 }
-#if defined(__GNUG__)
+
+#if defined(__INTEL_COMPILER)
+//#pragma warning ( disable : 383 )
+#elif defined (__clang__) // test clang to be sure that when we test for gnu it is only gnu
+#elif defined(__GNUG__)
 #  if GCC_VERSION > 40600
 #    pragma GCC diagnostic pop
 #  else
@@ -246,28 +253,28 @@ template<>
 inline void aka_geev<double>(char* jobvl, char* jobvr, int* n, double* a,
                              int* lda, double* wr, double* wi, double* vl, int* ldvl,
                              double* vr, int* ldvr, double* work, int* lwork, int* info) {
-AKA_FC_GLOBAL(dgeev, DGEEV)(jobvl, jobvr, n, a,
-                            lda, wr, wi, vl, ldvl,
-                            vr, ldvr, work, lwork, info);
+  AKA_FC_GLOBAL(dgeev, DGEEV)(jobvl, jobvr, n, a,
+			      lda, wr, wi, vl, ldvl,
+			      vr, ldvr, work, lwork, info);
 }
 
 template<>
 inline void aka_getrf<double>(int* m, int *n,
-		       double* a, int* lda,
-		       int* ipiv, int* info) {
+			      double* a, int* lda,
+			      int* ipiv, int* info) {
   AKA_FC_GLOBAL(dgetrf, DGETRF)(m, n, a, lda, ipiv, info);
 }
 
 template<>
 inline void aka_getri<double>(int* n, double* a, int* lda,
-                       int* ipiv, double* work, int* lwork, int* info) {
+			      int* ipiv, double* work, int* lwork, int* info) {
   AKA_FC_GLOBAL(dgetri, DGETRI)(n, a, lda, ipiv, work, lwork, info);
 }
 
 template<>
 inline void aka_getrs<double>(char *trans, int * n, int * nrhs,
-		       double * A, int * lda, int * ipiv,
-		       double * b, int * ldb, int * info) {
+			      double * A, int * lda, int * ipiv,
+			      double * b, int * ldb, int * info) {
   AKA_FC_GLOBAL(dgetrs, DGETRS)(trans, n, nrhs, A, lda, ipiv, b, ldb, info);
 }
 
@@ -275,30 +282,30 @@ inline void aka_getrs<double>(char *trans, int * n, int * nrhs,
 /* -------------------------------------------------------------------------- */
 template<>
 inline void aka_geev<float>(char* jobvl, char* jobvr, int* n, float* a,
-			       int* lda, float* wr, float* wi, float* vl, int* ldvl,
-			       float* vr, int* ldvr, float* work, int* lwork, int* info) {
-AKA_FC_GLOBAL(sgeev, SGEEV)(jobvl, jobvr, n, a,
+			    int* lda, float* wr, float* wi, float* vl, int* ldvl,
+			    float* vr, int* ldvr, float* work, int* lwork, int* info) {
+  AKA_FC_GLOBAL(sgeev, SGEEV)(jobvl, jobvr, n, a,
                               lda, wr, wi, vl, ldvl,
                               vr, ldvr, work, lwork, info);
 }
 
 template<>
 inline void aka_getrf<float>(int* m, int *n,
-		       float* a, int* lda,
-		       int* ipiv, int* info) {
+			     float* a, int* lda,
+			     int* ipiv, int* info) {
   AKA_FC_GLOBAL(sgetrf, SGETRF)(m, n, a, lda, ipiv, info);
 }
 
 template<>
 inline void aka_getri<float>(int* n, float* a, int* lda,
-                       int* ipiv, float* work, int* lwork, int* info) {
+			     int* ipiv, float* work, int* lwork, int* info) {
   AKA_FC_GLOBAL(sgetri, SGETRI)(n, a, lda, ipiv, work, lwork, info);
 }
 
 template<>
 inline void aka_getrs<float>(char *trans, int * n, int * nrhs,
-		       float * A, int * lda, int * ipiv,
-		       float * b, int * ldb, int * info) {
+			     float * A, int * lda, int * ipiv,
+			     float * b, int * ldb, int * info) {
   AKA_FC_GLOBAL(sgetrs, SGETRS)(trans, n, nrhs, A, lda, ipiv, b, ldb, info);
 }
 #endif
