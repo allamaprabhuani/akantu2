a0111e011fc511d63832f29caf2dc0a5658da339
Pino Muñoz Daniel <daniel.pinomunoz@epfl.ch>
2014-06-06 12:27:53 +0000

    Correction made with Nicolas to correct compilation error for Boost <= 1.48 and also remove compilation warning for GCC <= 4.8

--- src/mesh_utils/mesh_graph.hh
+++ src/mesh_utils/mesh_graph.hh
@@ -38,10 +38,10 @@
 #elif defined (__clang__) // test clang to be sure that when we test for gnu it is only gnu
 #elif (defined(__GNUC__) || defined(__GNUG__))
 #  define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
-#  if GCC_VERSION > 40600
+#  if GCC_VERSION > 40800
 #    pragma GCC diagnostic push
+#    pragma GCC diagnostic ignored "-Wunused-local-typedefs"
 #  endif
-#  pragma GCC diagnostic ignored "-Wunused-local-typedefs"
 #endif
 
 
@@ -153,10 +153,8 @@ private:
 //#pragma warning ( disable : 383 )
 #elif defined (__clang__) // test clang to be sure that when we test for gnu it is only gnu
 #elif defined(__GNUG__)
-#  if GCC_VERSION > 40600
+#  if GCC_VERSION > 40800
 #    pragma GCC diagnostic pop
-#  else
-#    pragma GCC diagnostic warning "-Wunused-local-typedefs"
 #  endif
 #endif
 
