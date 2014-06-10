8a4f84c8d8850bae3eb571489e9da5f2fa2cc88d
Anciaux Guillaume <guillaume.anciaux@epfl.ch>
2014-06-10 16:29:16 +0000

    advancing the manual

---
+++ packages/00_doxygen_documentation.cmake
@@ -0,0 +1,6 @@
+option(AKANTU_DOXYGEN_DOCUMENTATION "Build source documentation using Doxygen." OFF)
+
+#set(AKANTU_DOXYGEN_DOCUMENTATION_DOCUMENTATION 
+#"
+#This 
+#")
---
+++ packages/00_manual_documentation.cmake
@@ -0,0 +1,7 @@
+option(AKANTU_DOXYGEN_DOCUMENTATION "Build source documentation using Doxygen." OFF)
+option(AKANTU_MANUAL_DOCUMENTATION  "Build manual." OFF)
+
+set(AKANTU_DOCUMENTATION_DOCUMENTATION 
+"
+This 
+")
