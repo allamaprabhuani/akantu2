1f6ef5a3a5d42bcd44b51d2c06f140b42d9c201f
Mohit Pundir <mohit.pundir@epfl.ch>
2018-09-10 16:49:59 +0200

    contact mechanics model started

---
+++ src/model/contact_mechanics/contact_mechanics_model.hh
@@ -0,0 +1,37 @@
+/**
+ * @file   contact_mechanics_model.hh
+ *
+ * @author Mohit Pundir <mohit.pundir@epfl.ch>
+ *
+ * @date creation: Tue Sep 10 2018
+ * @date last modification: Mon Sep 10 2018
+ *
+ * @brief  Model of Contact Mechanics
+ *
+ * @section LICENSE
+ *
+ * Copyright (©)  2010-2018 EPFL (Ecole Polytechnique Fédérale de Lausanne)
+ * Laboratory (LSMS - Laboratoire de Simulation en Mécanique des Solides)
+ *
+ * Akantu is free  software: you can redistribute it and/or  modify it under the
+ * terms  of the  GNU Lesser  General Public  License as published by  the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Akantu is  distributed in the  hope that it  will be useful, but  WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See  the GNU  Lesser General  Public License  for more
+ * details.
+ *
+ * You should  have received  a copy  of the GNU  Lesser General  Public License
+ * along with Akantu. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#ifndef __AKANTU_CONTACT_MECHANICS_MODEL_HH__
+#define __AKANTU_CONTACT_MECHANICS_MODEL_HH__
+
+
+
+#endif
