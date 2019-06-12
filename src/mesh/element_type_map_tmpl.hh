cce3dea5210ce1800cca9178575d94ea7ebd042d
Nicolas Richart <nicolas.richart@epfl.ch>
2019-06-12 10:59:34 +0200

    potential bug

--- src/mesh/element_type_map_tmpl.hh
+++ src/mesh/element_type_map_tmpl.hh
@@ -169,7 +169,7 @@ void ElementTypeMapArray<T, SupportType>::copy(
     const ElementTypeMapArray & other) {
   for (auto ghost_type : ghost_types) {
     for (auto type :
-         this->elementTypes(_all_dimensions, ghost_types, _ek_not_defined)) {
+         this->elementTypes(_all_dimensions, ghost_type, _ek_not_defined)) {
       const auto & array_to_copy = other(type, ghost_type);
       auto & array =
           this->alloc(0, array_to_copy.getNbComponent(), type, ghost_type);
