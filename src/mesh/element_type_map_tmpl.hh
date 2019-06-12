976b85225a2168b6ff6ce452844ca0ca38004fb7
Nicolas Richart <nicolas.richart@epfl.ch>
2019-06-12 11:01:34 +0200

    clang-format

--- src/mesh/element_type_map_tmpl.hh
+++ src/mesh/element_type_map_tmpl.hh
@@ -414,7 +414,7 @@ inline typename ElementTypeMap<Stored, SupportType>::type_iterator::reference
 /* -------------------------------------------------------------------------- */
 template <class Stored, typename SupportType>
 inline typename ElementTypeMap<Stored, SupportType>::type_iterator &
-    ElementTypeMap<Stored, SupportType>::type_iterator::operator++() {
+ElementTypeMap<Stored, SupportType>::type_iterator::operator++() {
   ++list_begin;
   while ((list_begin != list_end) &&
          (((dim != _all_dimensions) &&
@@ -428,7 +428,7 @@ inline typename ElementTypeMap<Stored, SupportType>::type_iterator &
 /* -------------------------------------------------------------------------- */
 template <class Stored, typename SupportType>
 typename ElementTypeMap<Stored, SupportType>::type_iterator
-    ElementTypeMap<Stored, SupportType>::type_iterator::operator++(int) {
+ElementTypeMap<Stored, SupportType>::type_iterator::operator++(int) {
   type_iterator tmp(*this);
   operator++();
   return tmp;
