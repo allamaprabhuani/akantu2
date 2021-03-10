71421fe2da4165033d93ad4d9114ea13a7ec5f32
Emil Gallyamov <emil.gallyamov@epfl.ch>
2021-03-10 19:11:19 +0100

    Experimental!!! Cohesive normals always computed in initial config

--- src/model/solid_mechanics/solid_mechanics_model_cohesive/materials/material_cohesive.cc
+++ src/model/solid_mechanics/solid_mechanics_model_cohesive/materials/material_cohesive.cc
@@ -288,8 +288,11 @@ void MaterialCohesive::assembleStiffnessMatrix(GhostType ghost_type) {
         nb_element * nb_quadrature_points,
         spatial_dimension * spatial_dimension, "tangent_stiffness_matrix");
 
-    computeNormal(model->getCurrentPosition(), normals(type, ghost_type), type,
+    computeNormal(model->getMesh().getNodes(), normals(type, ghost_type), type,
                   ghost_type);
+    // computeNormal(model->getCurrentPosition(), normals(type, ghost_type),
+    // type,
+    //               ghost_type);
 
     /// compute openings @f$\mathbf{\delta}@f$
     // computeOpening(model->getDisplacement(), opening(type, ghost_type), type,
@@ -394,8 +397,11 @@ void MaterialCohesive::computeTraction(GhostType ghost_type) {
     }
 
     /// compute normals @f$\mathbf{n}@f$
-    computeNormal(model->getCurrentPosition(), normals(type, ghost_type), type,
+    computeNormal(model->getMesh().getNodes(), normals(type, ghost_type), type,
                   ghost_type);
+    // computeNormal(model->getCurrentPosition(), normals(type, ghost_type),
+    // type,
+    //               ghost_type);
 
     /// compute openings @f$\mathbf{\delta}@f$
     computeOpening(model->getDisplacement(), opening(type, ghost_type), type,
