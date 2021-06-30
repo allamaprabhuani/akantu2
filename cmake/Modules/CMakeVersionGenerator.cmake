8110c322b1e82bd643d8f578367efdbf1f4072a6
Nicolas Richart <nicolas.richart@epfl.ch>
2021-06-30 23:13:18 +0200

    Cleaning cmake files

--- cmake/Modules/CMakeVersionGenerator.cmake
+++ cmake/Modules/CMakeVersionGenerator.cmake
@@ -36,52 +36,103 @@ if(__DEFINE_PROJECT_VERSION__)
 endif()
 set(__DEFINE_PROJECT_VERSION__ TRUE)
 
-macro(define_project_version)
-  string(TOUPPER ${PROJECT_NAME} _project)
+function(_get_version_from_git)
+  find_package(Git)
+  if(Git_FOUND)
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} describe
+        --tags
+        --dirty
+        --always
+        --long
+        --match v*
+      WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+      RESULT_VARIABLE _res
+      OUTPUT_VARIABLE _out
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-  if(EXISTS ${PROJECT_SOURCE_DIR}/VERSION)
-    file(STRINGS ${PROJECT_SOURCE_DIR}/VERSION ${_project}_VERSION)
+    # git describe to PEP404 version
+    set(_version_regex
+      "^v([0-9.]+)(-([0-9]+)-g([0-9a-f]+)(-dirty)?)?$")
 
-    if("${${_project}_VERSION}" MATCHES "^([0-9]+)")
-      string(REGEX REPLACE "^([0-9]+).*" "\\1" _ver_major "${${_project}_VERSION}")
-      set(${_project}_MAJOR_VERSION ${_ver_major})
+    if(_out MATCHES ${_version_regex})
+      set(_version ${CMAKE_MATCH_1} PARENT_SCOPE)
+      if(CMAKE_MATCH_2)
+        set(_metadata "${CMAKE_MATCH_3}.${CMAKE_MATCH_4}")
+      endif()
+      if(CMAKE_MATCH_5)
+        set(_metadata "${_metadata}.dirty")
+      endif()
+    else()
+      execute_process(
+        COMMAND ${GIT_EXECUTABLE} rev-list HEAD --count
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+        RESULT_VARIABLE _res
+        OUTPUT_VARIABLE _out_count
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-      if("${${_project}_VERSION}" MATCHES "^${_ver_major}\\.([0-9]+)")
-        string(REGEX REPLACE "^${_ver_major}\\.([0-9]+).*" "\\1" _ver_minor "${${_project}_VERSION}")
-        set(${_project}_MINOR_VERSION ${_ver_minor})
-        if("${${_project}_VERSION}" MATCHES "^${_ver_major}\\.${_ver_minor}\\.([0-9a-zA-Z\\-]+)")
-          string(REGEX REPLACE "^${_ver_major}\\.${_ver_minor}\\.([0-9a-zA-Z\\-]+).*" "\\1" _ver_build "${${_project}_VERSION}")
-          set(${_project}_BUILD_VERSION ${_ver_build})
+      if(_out MATCHES "^([0-9a-f]+)(-dirty)?$")
+        set(_metadata "${CMAKE_MATCH_1}")
+        if(_res EQUAL 0)
+          set(_metadata "${_out_count}.${_version_metadata}")
+        endif()
+
+        if(CMAKE_MATCH_2)
+          set(_metadata "${_version_metadata}.dirty")
         endif()
       endif()
     endif()
-  # else()
-  #   find_package(Subversion)
+    set(_version_metadata ${_metadata} PARENT_SCOPE)
+  endif()
+endfunction()
 
-  #   if(SUBVERSION_FOUND)
-  #     subversion_wc_info(${PROJECT_SOURCE_DIR} ${_project} ERROR_QUIET)
-  #     if(${${_project}_WC_FOUND})
-  #       set(${_project}_BUILD_VERSION ${${_project}_WC_REVISION})
-  #       set(${_project}_VERSION
-  #         "${${_project}_MAJOR_VERSION}.${${_project}_MINOR_VERSION}.${${_project}_BUILD_VERSION}"
-  #         )
-  #     endif()
-  #   endif()
+function(_get_version_from_file)
+  if(EXISTS ${PROJECT_SOURCE_DIR}/VERSION)
+    file(STRINGS ${PROJECT_SOURCE_DIR}/VERSION _file_version)
+    if("_file_version" MATCHES "^([0-9]+(\\.[0-9]+)?(\\.[0-9]+)?)(\\+(.*))?")
+      set(_version ${CMAKE_MATCH_1} PARENT_SCOPE)
+      if(CMAKE_MATCH_4)
+        set(_version_metadata ${CMAKE_MATCH_4} PARENT_SCOPE)
+      endif()
+    endif()
+  endif()
+endfunction()
+
+function(define_project_version)
+  string(TOUPPER ${PROJECT_NAME} _project)
+
+  _get_version_from_git()
+  if(NOT _version)
+    _get_version_from_file()
   endif()
 
-  if(NOT ${_project}_VERSION)
-    set(${_project}_VERSION
-      "${${_project}_MAJOR_VERSION}.${${_project}_MINOR_VERSION}"
-      )
+  if(_version)
+    set(${_project}_VERSION ${_version} PARENT_SCOPE)
+    if(_version_metadata)
+      set(${_project}_SEMVER "${_version}+${_version_metadata}" PARENT_SCOPE)
+      message(STATUS "${PROJECT_NAME} version: ${_version}+${_version_metadata}")
+    else()
+      message(STATUS "${PROJECT_NAME} version: ${_version}")
+    endif()
+
+    if(_version MATCHES "([0-9]+)(\\.([0-9]+))?(\\.([0-9]+))?")
+      set(_major_version ${CMAKE_MATCH_1})
+      set(${_project}_MAJOR_VERSION ${_major_version} PARENT_SCOPE)
+      if(CMAKE_MATCH_2)
+        set(_minor_version ${CMAKE_MATCH_3})
+        set(${_project}_MINOR_VERSION ${_minor_version} PARENT_SCOPE)
+      endif()
+      if(CMAKE_MATCH_4)
+        set(_patch_version ${CMAKE_MATCH_5})
+        set(${_project}_PATCH_VERSION ${_patch_version} PARENT_SCOPE)
+      endif()
+    endif()
   endif()
 
-  # Append the library version information to the library target properties
   if(NOT ${_project}_NO_LIBRARY_VERSION)
-    message(STATUS "${PROJECT_NAME} version: ${${_project}_VERSION}")
-
     set(${_project}_LIBRARY_PROPERTIES ${${_project}_LIBRARY_PROPERTIES}
-      VERSION "${${_project}_VERSION}"
-      SOVERSION "${${_project}_MAJOR_VERSION}.${${_project}_MINOR_VERSION}"
+      VERSION "${_version}"
+      SOVERSION "${_major_version}.${_minor_version}"
       )
   endif()
-endmacro()
+endfunction()
