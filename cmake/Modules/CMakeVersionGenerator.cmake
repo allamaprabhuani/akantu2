3d9e8d2ee369530577be56d6dc55771dffcea24c
Nicolas Richart <nicolas.richart@epfl.ch>
2022-01-19 14:31:50 +0000

    Correcting the tests for the python package

--- cmake/Modules/CMakeVersionGenerator.cmake
+++ cmake/Modules/CMakeVersionGenerator.cmake
@@ -38,7 +38,7 @@ set(__DEFINE_PROJECT_VERSION__ TRUE)
 
 function(_match_semver _input_semver prefix)
   set(_semver_regexp
-    "^([0-9]+(\\.[0-9]+)?(\\.[0-9]+)?)(-([a-zA-Z0-9-]*))?(\\+(.*))?")
+    "^([0-9]+(\\.[0-9]+)?(\\.[0-9]+)?)(-([a-zA-Z0-9\-]*))?(\\+(.*))?")
 
   if(_input_semver MATCHES "^([0-9]+(\\.[0-9]+)?(\\.[0-9]+)?)(-([a-zA-Z0-9-]*))?(\\+(.*))?")
     set(${prefix}_version ${CMAKE_MATCH_1} PARENT_SCOPE)
@@ -60,79 +60,82 @@ function(_get_version_from_git)
 
   find_package(Git)
 
-  if(Git_FOUND)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} describe
-        --tags
-        --abbrev=0
-        --match ${CMAKE_VERSION_GENERATOR_TAG_PREFIX}*
-      WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      RESULT_VARIABLE _res
-      OUTPUT_VARIABLE _out_tag
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      ERROR_VARIABLE _err_tag)
+  if(NOT Git_FOUND)
+    return()
+  endif()
 
-    if(NOT _res EQUAL 0)
-      return()
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} describe
+    --tags
+    --abbrev=0
+    --match ${CMAKE_VERSION_GENERATOR_TAG_PREFIX}*
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    RESULT_VARIABLE _res
+    OUTPUT_VARIABLE _out_tag
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_VARIABLE _err_tag)
+
+  if(NOT _res EQUAL 0)
+    return()
+  endif()
+
+  string(REGEX REPLACE "^${CMAKE_VERSION_GENERATOR_TAG_PREFIX}(.*)" "\\1" _tag "${_out_tag}")
+
+  _match_semver("${_tag}" _tag)
+
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} describe
+    --tags
+    --dirty
+    --always
+    --long
+    --match ${CMAKE_VERSION_GENERATOR_TAG_PREFIX}*
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    RESULT_VARIABLE _res
+    OUTPUT_VARIABLE _out
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+  set(_git_version ${_tag_version} PARENT_SCOPE)
+
+  if(_tag_version_prerelease)
+    set(_git_version_prerelease ${_tag_version_prerelease} PARENT_SCOPE)
+  endif()
+
+  # git describe to PEP404 version
+  set(_version_regex
+    "^${CMAKE_VERSION_GENERATOR_TAG_PREFIX}${_tag}(-([0-9]+)-(g[0-9a-f]+)(-dirty)?)?$")
+
+  if(_out MATCHES ${_version_regex})
+    if(CMAKE_MATCH_1)
+      if(_tag_version_metadata)
+        set(_metadata "${_tag_version_metadata}.")
+      endif()
+      set(_metadata "${_metadata}${CMAKE_MATCH_2}.${CMAKE_MATCH_3}")
     endif()
-
-    string(REGEX REPLACE "^${CMAKE_VERSION_GENERATOR_TAG_PREFIX}(.*)" "\\1" _tag "${_out_tag}")
-
-    _match_semver("${_tag}" _tag)
-
+    if(CMAKE_MATCH_4)
+      set(_metadata "${_metadata}.dirty")
+    endif()
+  else()
     execute_process(
-      COMMAND ${GIT_EXECUTABLE} describe
-        --tags
-        --dirty
-        --always
-        --long
-        --match ${CMAKE_VERSION_GENERATOR_TAG_PREFIX}*
+      COMMAND ${GIT_EXECUTABLE} rev-list HEAD --count
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
       RESULT_VARIABLE _res
-      OUTPUT_VARIABLE _out
+      OUTPUT_VARIABLE _out_count
       OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-    set(_git_version ${_tag_version} PARENT_SCOPE)
-
-    if(_tag_version_prerealease)
-      set(_git_version_prerelease ${_tag_version_prerealease} PARENT_SCOPE)
-    endif()
-
-    # git describe to PEP404 version
-    set(_version_regex
-      "^${CMAKE_VERSION_GENERATOR_TAG_PREFIX}${_tag}(-([0-9]+)-(g[0-9a-f]+)(-dirty)?)?$")
-
-    if(_out MATCHES ${_version_regex})
-      if(CMAKE_MATCH_1)
-        if(_tag_version_metadata)
-          set(_metadata "${_tag_version_metadata}.")
-        endif()
-        set(_metadata "${_metadata}${CMAKE_MATCH_2}.${CMAKE_MATCH_3}")
+    if(_out MATCHES "^([0-9a-f]+)(-dirty)?$")
+      set(_metadata "${CMAKE_MATCH_1}")
+      if(_res EQUAL 0)
+        set(_metadata "${_out_count}.${_metadata}")
       endif()
-      if(CMAKE_MATCH_4)
+
+      if(CMAKE_MATCH_2)
         set(_metadata "${_metadata}.dirty")
       endif()
-    else()
-      execute_process(
-        COMMAND ${GIT_EXECUTABLE} rev-list HEAD --count
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-        RESULT_VARIABLE _res
-        OUTPUT_VARIABLE _out_count
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-      if(_out MATCHES "^([0-9a-f]+)(-dirty)?$")
-        set(_metadata "${CMAKE_MATCH_1}")
-        if(_res EQUAL 0)
-          set(_metadata "${_out_count}.${_metadata}")
-        endif()
-
-        if(CMAKE_MATCH_2)
-          set(_metadata "${_metadata}.dirty")
-        endif()
-      endif()
     endif()
-    set(_git_version_metadata ${_metadata} PARENT_SCOPE)
   endif()
+  set(_git_version_metadata ${_metadata} PARENT_SCOPE)
+
 endfunction()
 
 function(_get_version_from_file)
