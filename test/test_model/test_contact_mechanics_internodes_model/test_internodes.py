92b7d9b7e2faf04f7452ea7b22e80c0cefeb092d
FMatti <somecallmefabio@gmail.com>
2022-12-08 15:01:46 +0100

    Add contact detection tests to reference code

--- test/test_model/test_contact_mechanics_internodes_model/test_internodes.py
+++ test/test_model/test_contact_mechanics_internodes_model/test_internodes.py
@@ -24,7 +24,7 @@ from init_model import init_model
 from example_direct import solve_step_direct
 
 
-def reference_setup():
+def reference_setup(d=0.1):
     mesh_file = 'contact.msh'
     material_file = file_prefix + 'material.dat'
 
@@ -45,10 +45,10 @@ def reference_setup():
     model.applyBC(aka.FixedValue(0., aka._y), 'lower_bottom')
 
     # Dirichlet
-    model.applyBC(aka.FixedValue(-0.1, aka._y), 'upper_top') # to block the nodes
+    model.applyBC(aka.FixedValue(-d, aka._y), 'upper_top') # to block the nodes
     nodes_top = mesh.getElementGroup('upper_top').getNodeGroup().getNodes().ravel()
     displacements = displacements.reshape([-1, 2])
-    displacements[nodes_top, 1] = -0.1
+    displacements[nodes_top, 1] = -d
     displacements = displacements.ravel()
 
     # Neumann (K is not invertible)
@@ -207,6 +207,188 @@ def test_solveStep():
 
     np.testing.assert_allclose(positions_new, positions_new_ref)
 
+### TEST PYTHON REFERENCE IMPLEMENTATION
+
+def _get_uniform_interface_grid(dim, graph, x_min=-1, x_max=1, n_grid=10):
+    linspace = np.linspace(x_min, x_max, n_grid)
+    meshgrid = np.meshgrid(*[linspace]*(dim-1))
+    grid = np.c_[[meshgrid[i].ravel() for i in range(dim-1)]]
+    mesh = np.c_[grid.T, graph(grid)]
+    return mesh
+
+def _get_random_interface_grid(dim, graph, x_min=-1, x_max=1, n_grid=10, seed=0):
+    np.random.seed(0)
+    grid = np.random.uniform(x_min, x_max, (dim-1, n_grid**(dim-1)))
+    mesh = np.c_[grid.T, graph(grid)]
+    return mesh
+
+def _check_rbf_radius_conditions(positions, rbf_radius_parameters, c_min=0.49, c_max=0.95):
+    # Check condition: [1], Page 51, Section 2.3, Equation 2
+    dist_MM = sp.spatial.distance.cdist(positions, positions)
+    np.fill_diagonal(dist_MM, np.inf)
+    min_ratio = np.min(dist_MM / rbf_radius_parameters, axis=1)
+    np.testing.assert_array_less(c_min*np.ones_like(min_ratio),  min_ratio)
+
+    # Check condition: [1], Page 51, Section 2.3, Equation 4
+    n_supports = np.sum(dist_MM < rbf_radius_parameters, axis=0)
+    n_supports_max = np.ones_like(n_supports) / (1-c_max)**4*(1+4*c_max)
+    np.testing.assert_array_less(n_supports,  n_supports_max)
+
+def _check_node_contained_in_support(positions, positions_ref, rbf_radius_parameters, C_max=0.99):
+    # Check condition: [1], Page 51, Section 2.3, Equation 3
+    dist_MN = sp.spatial.distance.cdist(positions_ref, positions)
+    min_ratio = np.min(dist_MN / rbf_radius_parameters, axis=1)
+    np.testing.assert_array_less(min_ratio, C_max*np.ones_like(min_ratio))
+
+def test_compute_rbf_radius_parameters_regular():
+
+    dim = 2
+
+    g_zeros = lambda x: np.zeros(x.shape[1])
+    g_parabolic = lambda x: np.sum(x**2, axis=0) + 0.05
+    positions1i = _get_uniform_interface_grid(dim, g_zeros, n_grid=10)
+    positions2i = _get_uniform_interface_grid(dim, g_parabolic, n_grid=10)
+    nodes1i = np.arange(len(positions1i))
+    nodes2i = np.arange(len(positions2i))
+
+    radiuses1i, _ = compute_radiuses(nodes1i, nodes2i, positions1i, positions2i)
+    _check_rbf_radius_conditions(positions1i, radiuses1i)
+
+    radiuses2i, _ = compute_radiuses(nodes2i, nodes1i, positions2i, positions1i)
+    _check_rbf_radius_conditions(positions2i, radiuses2i)
+
+def test_compute_rbf_radius_parameters_random():
+
+    dim = 2
+
+    g_zeros = lambda x: np.zeros(x.shape[1])
+    g_parabolic = lambda x: np.sum(x**2, axis=0) + 0.05
+    positions1i = _get_random_interface_grid(dim, g_zeros, n_grid=10)
+    positions2i = _get_random_interface_grid(dim, g_parabolic, n_grid=10)
+    nodes1i = np.arange(len(positions1i))
+    nodes2i = np.arange(len(positions2i))
+
+    radiuses1i, _ = compute_radiuses(nodes1i, nodes2i, positions1i, positions2i)
+    _check_rbf_radius_conditions(positions1i, radiuses1i)
+
+    radiuses2i, _ = compute_radiuses(nodes2i, nodes1i, positions2i, positions1i)
+    _check_rbf_radius_conditions(positions2i, radiuses2i)
+
+def test_find_interface_nodes_regular():
+
+    dim = 2
+
+    g_zeros = lambda x: np.zeros(x.shape[1])
+    g_parabolic = lambda x: np.sum(x**2, axis=0) + 0.05
+    positions1b = _get_uniform_interface_grid(dim, g_zeros, n_grid=10)
+    positions2b = _get_uniform_interface_grid(dim, g_parabolic, n_grid=10)
+    nodes1b = np.arange(len(positions1b))
+    nodes2b = np.arange(len(positions2b))
+
+    nodes1i, nodes2i, positions1i, positions2i, radiuses1, radiuses2 = \
+    find_contact_nodes(nodes1b, nodes2b, positions1b, positions2b)
+
+    _check_node_contained_in_support(positions1i, positions2i, radiuses1)
+    _check_node_contained_in_support(positions2i, positions1i, radiuses2)
+
+def test_find_interface_nodes_random():
+
+    dim = 2
+
+    g_zeros = lambda x: np.zeros(x.shape[1])
+    g_parabolic = lambda x: np.sum(x**2, axis=0) + 0.05
+    positions1b = _get_random_interface_grid(dim, g_zeros, n_grid=10)
+    positions2b = _get_random_interface_grid(dim, g_parabolic, n_grid=10)
+    nodes1b = np.arange(len(positions1b))
+    nodes2b = np.arange(len(positions2b))
+
+    nodes1i, nodes2i, positions1i, positions2i, radiuses1, radiuses2 = \
+    find_contact_nodes(nodes1b, nodes2b, positions1b, positions2b)
+
+    _check_node_contained_in_support(positions1i, positions2i, radiuses1)
+    _check_node_contained_in_support(positions2i, positions1i, radiuses2)
+
+def test_construct_gap_function_interpolation_regular():
+
+    dim = 2
+
+    g_zeros = lambda x: np.zeros(x.shape[1])
+    g_parabolic = lambda x: np.sum(x**2, axis=0) + 0.05
+    positions1b = _get_uniform_interface_grid(dim, g_zeros, n_grid=10)
+    positions2b = _get_uniform_interface_grid(dim, g_parabolic, n_grid=10)
+    nodes1b = np.arange(len(positions1b))
+    nodes2b = np.arange(len(positions2b))
+
+    nodes1i, nodes2i, positions1i, positions2i, radiuses1, radiuses2 = \
+    find_contact_nodes(nodes1b, nodes2b, positions1b, positions2b)
+
+    R12, R21, _, _ = assemble_Rijs(positions1i, positions2i, radiuses1, radiuses2)
+
+    positions1i_interp = R12 @ positions2i
+    positions2i_interp = R21 @ positions1i
+
+    np.testing.assert_allclose(g_parabolic(positions1i_interp[:, :-1].T), positions1i_interp[:, -1], atol=2e-2)
+    np.testing.assert_allclose(g_zeros(positions2i_interp[:, :-1].T), positions2i_interp[:, -1], atol=2e-2)
+
+def test_construct_gap_function_interpolation_random():
+
+    dim = 2
+
+    g_zeros = lambda x: np.zeros(x.shape[1])
+    g_parabolic = lambda x: np.sum(x**2, axis=0) + 0.05
+    positions1b = _get_random_interface_grid(dim, g_zeros, n_grid=10)
+    positions2b = _get_random_interface_grid(dim, g_parabolic, n_grid=10)
+    nodes1b = np.arange(len(positions1b))
+    nodes2b = np.arange(len(positions2b))
+
+    nodes1i, nodes2i, positions1i, positions2i, radiuses1, radiuses2 = \
+    find_contact_nodes(nodes1b, nodes2b, positions1b, positions2b)
+
+    R12, R21, _, _ = assemble_Rijs(positions1i, positions2i, radiuses1, radiuses2)
+
+    positions1i_interp = R12 @ positions2i
+    positions2i_interp = R21 @ positions1i
+
+    np.testing.assert_allclose(g_parabolic(positions1i_interp[:, :-1].T), positions1i_interp[:, -1], atol=2e-2)
+    np.testing.assert_allclose(g_zeros(positions2i_interp[:, :-1].T), positions2i_interp[:, -1], atol=2e-2)
+
+def _get_theoretical_contact_radius(R, d):
+    return (d*R)**0.5
+
+def _get_theoretical_pressure_amplitude(R, d, E, nu):
+    a = _get_theoretical_contact_radius(R, d)
+    E_red = E/(2*(1-nu**2))
+    F = 4/3 * E_red * R**0.5 * d**1.5
+    return 3*F / (2*np.pi*a**2)
+
+def _get_theoretical_normal_displacement(R, d, E, nu):
+    a = _get_theoretical_contact_radius(R, d)
+    p0 = _get_theoretical_pressure_amplitude(R, d, E, nu)
+    return - (1-nu**2)/E * np.pi/2 * p0 * a
+
+def test_contact2d_problem():
+
+    # Model parameters
+    R = (1**2 + 0.5**2)**0.5
+    d0 = 1 - (2.1 - R)
+
+    d_list = np.linspace(0.05, 0.35, 5)
+    u_list = np.empty_like(d_list)
+
+    for i, d in enumerate(d_list):
+
+        model, mesh, data = reference_setup(d-d0)
+        positions_new, displacements_ref = solve_step_direct(model, data, nb_max_iter=10) 
+
+        nodes2i = mesh.getElementGroup("upper_bottom").getNodeGroup().getNodes().ravel()
+        positions2i = positions_new[nodes2i]
+
+        u_list[i] = np.min(positions2i[:, 1]) - 1
+
+    nu = model.getMaterial(0).getReal("nu")
+    E = model.getMaterial(0).getReal("E")
+
+    np.testing.assert_allclose(_get_theoretical_normal_displacement(R, d_list, E, nu), u_list, atol=5e-3)
 
 if __name__ == '__main__':
     pytest.main()
