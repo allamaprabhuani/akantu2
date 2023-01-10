e82bf8f701794f7a48b276b0fadb7d9b7f01188f
FMatti <somecallmefabio@gmail.com>
2023-01-10 18:11:08 +0100

    Remove redundant files and document tests

--- test/test_model/test_contact_mechanics_internodes_model/test_contact_detector_internodes.py
+++ test/test_model/test_contact_mechanics_internodes_model/test_contact_detector_internodes.py
@@ -4,10 +4,46 @@ import akantu as aka
 import scipy as sp
 
 def _wendland_rbf(distances, radiuses):
+    """
+    Evaluate the wendland radial basis function.
+
+        phi(d, r) = max{(1 - d/r)^4 * (1 + 4*d/r), 0}
+
+    Parameters
+    ----------
+    distances : np.ndarray
+        Distances between nodes.
+    radiuses : np.ndarray
+        Radius parameters for radial basis function.
+    """
     delta = distances / radiuses
     return (1 - delta) ** 4 * (1 + 4 * delta) * (delta <= 1)
 
 def _check_rbf_radius_conditions(positions, rbf_radius_parameters, c_min=0.05, c_max=0.95, n_samples=100):
+    """
+    Checks the conditions of the radial basis function are satisfied.
+
+    Parameters
+    ----------
+    positions : np.ndarray
+        Positions of interface nodes.
+    rbf_radius_parameters : np.ndarray
+        Radius parameters corresponding to the interface nodes.
+    c_min : float 
+        Minimum value to start search for 'c' satisfying the conditions.
+        'c' defined as used in [1], Page 51, Section 2.3, Equation 2.
+    c_max : float
+        Minimum value to stoop search for 'c' satisfying the conditions.
+    n_samples : int
+        Number of uniform steps from c_min to c_max in search for 'c'.
+
+    Reference
+    ---------
+    [1] Y. Voet et. al.: The INTERNODES method for applications in contact mechanics
+        and dedicated preconditioning techniques.
+        Computers & Mathematics with Applications, vol. 127, 2022, pp. 48-64
+        https://doi.org/10.1016/j.camwa.2022.09.019
+    """
     dist_MM = sp.spatial.distance.cdist(positions, positions)
     np.fill_diagonal(dist_MM, np.inf)
     min_ratio = np.min(dist_MM / rbf_radius_parameters, axis=1)
@@ -28,17 +64,95 @@ def _check_rbf_radius_conditions(positions, rbf_radius_parameters, c_min=0.05, c
     assert False, "No 'c' was found which satisfies radius parameter conditions"
 
 def _check_node_contained_in_support(positions, positions_ref, rbf_radius_parameters, C_max=0.99):
+    """
+    Checks the conditions of the radial basis function are satisfied.
+
+    Parameters
+    ----------
+    positions : np.ndarray
+        Positions of interface nodes.
+    positions_ref : np.ndarray
+        Position where the interpolant will be evaluated (opposing interface).
+    rbf_radius_parameters : np.ndarray
+        Radius parameters corresponding to the interface nodes.
+    C_max : float 
+        Maximum value constant 'C' in [1], Page 51, Section 2.3, Equation 3
+
+    Reference
+    ---------
+    [1] Y. Voet et. al.: The INTERNODES method for applications in contact mechanics
+        and dedicated preconditioning techniques.
+        Computers & Mathematics with Applications, vol. 127, 2022, pp. 48-64
+        https://doi.org/10.1016/j.camwa.2022.09.019
+    """
     # Check condition: [1], Page 51, Section 2.3, Equation 3
     dist_MN = sp.spatial.distance.cdist(positions_ref, positions)
     min_ratio = np.min(dist_MN / rbf_radius_parameters, axis=1)
     np.testing.assert_array_less(min_ratio, C_max*np.ones_like(min_ratio))
 
-def test_findContactNodes():
+def test_findContactNodes_2d():
+    """
+    Test if the node search finds radius parameters and interface nodes that
+    satisfy the conditions in [1] for a 2d problem of a plane interfacing with
+    a circle.
 
+    Reference
+    ---------
+    [1] Y. Voet et. al.: The INTERNODES method for applications in contact mechanics
+        and dedicated preconditioning techniques.
+        Computers & Mathematics with Applications, vol. 127, 2022, pp. 48-64
+        https://doi.org/10.1016/j.camwa.2022.09.019
+    """
     # Set up contact problem
     mesh_file = 'contact2d_plane_circle.msh'
-    material_file = 'contact2d_material.dat'
+    material_file = 'material.dat'
     spatial_dimension = 2
+    aka.parseInput(material_file)
+
+    # Read mesh
+    mesh = aka.Mesh(spatial_dimension)
+    mesh.read(mesh_file)
+
+    # Find contact nodes using detector
+    detector = aka.ContactDetectorInternodes(mesh)
+    detector.findContactNodes(detector.getMasterNodeGroup(), detector.getSlaveNodeGroup())
+
+    # Get interface nodes
+    nodes_interface_master = detector.getMasterNodeGroup().getNodes().ravel()
+    nodes_interface_slave = detector.getSlaveNodeGroup().getNodes().ravel()
+
+    # Get radius parameters of radial basis functions
+    radiuses_interface_master = detector.getMasterRadiuses().ravel()
+    radiuses_interface_slave = detector.getSlaveRadiuses().ravel()
+
+    # Get nodal positions
+    positions = mesh.getNodes()
+
+    # Check if radius parameters satisfy the conditions from the paper
+    _check_rbf_radius_conditions(positions[nodes_interface_master], radiuses_interface_master)
+    _check_rbf_radius_conditions(positions[nodes_interface_slave], radiuses_interface_slave)
+
+    # Check if all reference nodes are supported by an interpolation node
+    _check_node_contained_in_support(positions[nodes_interface_master], positions[nodes_interface_slave], radiuses_interface_master)
+    _check_node_contained_in_support(positions[nodes_interface_slave], positions[nodes_interface_master], radiuses_interface_slave)
+
+def test_findContactNodes_3d():
+    """
+    Test if the node search finds radius parameters and interface nodes that
+    satisfy the conditions in [1] for a 3d problem of a plane interfacing with
+    a sphere.
+
+    Reference
+    ---------
+    [1] Y. Voet et. al.: The INTERNODES method for applications in contact mechanics
+        and dedicated preconditioning techniques.
+        Computers & Mathematics with Applications, vol. 127, 2022, pp. 48-64
+        https://doi.org/10.1016/j.camwa.2022.09.019
+    """
+    # Set up contact problem
+    mesh_file = 'contact3d_plane_sphere.msh'
+    material_file = 'material.dat'
+    spatial_dimension = 3
     aka.parseInput(material_file)
 
     # Read mesh
--- test/test_model/test_contact_mechanics_internodes_model/test_contact_mechanics_internodes_model.py
+++ test/test_model/test_contact_mechanics_internodes_model/test_contact_mechanics_internodes_model.py
@@ -4,25 +4,81 @@ import akantu as aka
 import scipy as sp
 
 def _get_theoretical_contact_radius(R, d):
+    """
+    Obtain the theoretical contact radius for a Hertzian problem between a
+    sphere and a plane.
+
+    Parameters
+    ----------
+    R : float
+        Radius of the circle.
+    d : float
+        Vertical displacement.
+
+    Reference
+    ---------
+    [1] K. L. Johnson: Contact Mechanics. Cambridge university press, 1987.
+    """
     return (d * R) ** 0.5
 
 def _get_theoretical_pressure_amplitude(R, d, E, nu):
+    """
+    Obtain the theoretical pressure amplitude for a Hertzian problem between a
+    sphere and a plane.
+
+    Parameters
+    ----------
+    R : float
+        Radius of the circle.
+    d : float
+        Vertical displacement.
+    E : float
+        Young's modulus of the material.
+    nu : float
+        Poisson ratio of the material.
+
+    Reference
+    ---------
+    [1] K. L. Johnson: Contact Mechanics. Cambridge university press, 1987.
+    """
     a = _get_theoretical_contact_radius(R, d)
     E_red = E / (2 * (1 - nu**2))
     F = 4 / 3 * E_red * R**0.5 * d**1.5
     return 3 * F / (2 * np.pi * a**2)
 
 def _get_theoretical_normal_displacement(R, d, E, nu):
+    """
+    Obtain the theoretical normal displacement for a Hertzian problem between a
+    sphere and a plane.
+
+    Parameters
+    ----------
+    R : float
+        Radius of the circle.
+    d : float
+        Vertical displacement.
+    E : float
+        Young's modulus of the material.
+    nu : float
+        Poisson ratio of the material.
+
+    Reference
+    ---------
+    [1] K. L. Johnson: Contact Mechanics. Cambridge university press, 1987.
+    """
     a = _get_theoretical_contact_radius(R, d)
     p0 = _get_theoretical_pressure_amplitude(R, d, E, nu)
     K = np.pi / 2
     return -(1 - nu**2) / E * K * p0 * a
 
 def test_contact2d_plane_circle():
-
+    """
+    Test if the solution obtained with the internodes method satisfies the
+    theoretical expectation. 2d interface of a circle with a plane.
+    """
     # Set up contact problem
     mesh_file = 'contact2d_plane_circle.msh'
-    material_file = 'contact2d_material.dat'
+    material_file = 'material.dat'
     spatial_dimension = 2
     aka.parseInput(material_file)
 
@@ -64,10 +120,12 @@ def test_contact2d_plane_circle():
     np.testing.assert_allclose(_get_theoretical_contact_radius(R, d), a, atol=5e-2)
 
 def test_contact2d_circle_circle():
-
+    """
+    Test if the solution of two interfacing circles yields a planar interface.
+    """
     # Set up contact problem
     mesh_file = 'contact2d_circle_circle.msh'
-    material_file = 'contact2d_material.dat'
+    material_file = 'material.dat'
     spatial_dimension = 2
     aka.parseInput(material_file)
 
@@ -101,16 +159,19 @@ def test_contact2d_circle_circle():
     np.testing.assert_allclose(positions_interface_secondary[:, 1], np.zeros(len(positions_interface_secondary)), atol=1e-2)
 
 def test_contact3d_plane_sphere():
-
+    """
+    Test if the solution obtained with the internodes method satisfies the
+    theoretical expectation. 3d interface of a sphere with a plane.
+    """
     # Set up contact problem
     mesh_file = 'contact3d_plane_sphere.msh'
-    material_file = 'contact2d_material.dat'
+    material_file = 'material.dat'
     spatial_dimension = 3
     aka.parseInput(material_file)
 
     d0 = 0.05  # Initial overlap between primary and secondary mesh
-    R = 0.5  # Radius of circle
-    d = 0.1  # Displacement enforced on top of circle
+    R = 0.5  # Radius of sphere
+    d = 0.1  # Displacement enforced on top of sphere
 
     # Read mesh
     mesh = aka.Mesh(spatial_dimension)
@@ -148,15 +209,17 @@ def test_contact3d_plane_sphere():
     np.testing.assert_allclose(_get_theoretical_contact_radius(R, d), a, atol=5e-2)
 
 def test_contact3d_sphere_sphere():
-
+    """
+    Test if the solution of two interfacing spheres yields a planar interface.
+    """
     # Set up contact problem
     mesh_file = 'contact3d_sphere_sphere.msh'
-    material_file = 'contact2d_material.dat'
+    material_file = 'material.dat'
     spatial_dimension = 3
     aka.parseInput(material_file)
 
     d0 = 0.05  # Initial overlap between primary and secondary mesh
-    d = 0.1  # Displacement enforced on top of circle
+    d = 0.1  # Displacement enforced on top of sphere
 
     mesh = aka.Mesh(spatial_dimension)
     mesh.read(mesh_file)
