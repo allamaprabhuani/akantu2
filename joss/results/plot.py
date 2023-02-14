58a442c2f8b7a821415a3261bcecbd6a990f9259
Lucas Frérot <lucas.frerot@imtek.uni-freiburg.de>
2023-02-14 13:30:56 +0100

    joss: tts data on one plot

--- joss/results/plot.py
+++ joss/results/plot.py
@@ -2,118 +2,67 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib
-
-matplotlib.use("TKAgg")
 import matplotlib.pyplot as plt
+
+from matplotlib.lines import Line2D
+from matplotlib.patches import Patch
 
 # Same font as JOSS
 plt.rcParams["font.sans-serif"] = "cmss10"
 
-print("Using:", matplotlib.get_backend())
-plots = {
-    "elastic_gcc": {"prefix": "timmings_", "suffix": "_jed"},
-    "cohesive_gcc": {"prefix": "timmings_", "suffix": "_jed"},
-}
+# Loading data
+elastic = pd.read_csv("timmings_elastic_gcc_jed.csv", sep=",",
+                      skipinitialspace=True)
+cohesive = pd.read_csv("timmings_cohesive_gcc_jed.csv", sep=",",
+                       skipinitialspace=True)
+
+# Solve time
+elastic["step"] = elastic["solve_step"]
+cohesive["step"] = cohesive["check_cohesive_stress"] + cohesive["solve_step"]
+
+fig, ax = plt.subplots(figsize=(4, 3.5))
 
 
-axes = {}
-fig, (axes["elastic"], axes["cohesive"]) = plt.subplots(2, 1, sharex=True)
-handles = []
-for plot_name, data in plots.items():
-    mat, compiler = plot_name.split("_")
-    data["df"] = pd.read_csv(
-        f"""{data["prefix"]}{plot_name}{data["suffix"]}.csv""",
-        sep=",",
-        skipinitialspace=True,
-    )
+def plot_tts(ax, df, **kwargs):
+    g = df.groupby("psize")  # compute stats grouped by number of procs
+    med = g.median()
+    min = g.min()
+    max = g.max()
+    l, = ax.plot(med.index, med["step"], **kwargs)
+    ax.fill_between(med.index,
+                    min["step"],
+                    max["step"],
+                    color=l.get_color(),
+                    alpha=.2)
+    ax.plot(med.index, med["step"][1] / med.index, ls='--', color=l.get_color())
 
-plotting = "TTS"
 
-psize = np.array(sorted(set(plots[list(plots.keys())[0]]["df"]["psize"])))
+plot_tts(ax, cohesive, marker="s", label="insertion (median)")
+plot_tts(ax, elastic, marker="o", label="no insertion (median)")
 
-for plot_name, data in plots.items():
-    mat, compiler = plot_name.split("_")
-    df = data["df"]
+ax.set_xscale("log", base=2)
+ax.set_yscale("log")
 
-    measures = {}
-    if mat == "elastic":
-        step = df["solve_step"]
-    elif mat == "cohesive":
-        step = df["check_cohesive_stress"] + df["solve_step"]
-    measures["TTS"] = step * df["solve_step nb_rep"]
-    measures["speedup"] = step[0] / step
-    measures["mumps"] = df["static_solve"]
+ax.set_xlabel("Nb Cores [-]")
+ax.set_ylabel("Time to Solution [s]")
 
-    for name, measure in measures.items():
-        data[name] = {
-            "values": measure,
-            "median": [],
-            "mean": [],
-            "std": [],
-            "max": [],
-            "min": [],
-            "grouped": [],
-            "nb_measure": [],
-        }
-        for s in psize:
-            values = measure[df["psize"] == s]
-            data[name]["std"].append(values.std())
-            data[name]["mean"].append(values.mean())
-            data[name]["median"].append(values.median())
-            data[name]["max"].append(values.max())
-            data[name]["min"].append(values.min())
-            data[name]["grouped"].append(values)
-            data[name]["nb_measure"].append(len(values))
+# Selecting appropriate tick values
+psize = np.sort(np.unique(elastic["psize"]))
+psize = np.concatenate([[psize[0]],
+                        psize[1:][psize[1:] >= 2 * psize[:-1]],
+                        [psize[-1]]])
+ax.set_xticks(ticks=psize, labels=map(str, psize))
 
-    # axes[mat].boxplot(
-    #     data[plotting]["grouped"], positions=psize, widths=[0.1 * s for s in psize]
-    # )
+# Constructing legend with min/max and ideal labels
+handles, labels = ax.get_legend_handles_labels()
+handles += [
+    Line2D([], [], linestyle='--', color='k'),
+    Patch(color='k', alpha=.2),
+]
+labels += ["ideal", "min/max"]
+ax.legend(handles=handles, labels=labels)
 
-    print(f"""{mat}: {data[plotting]["nb_measure"]}\n {psize}""")
-    axes[mat].plot(
-        psize,
-        data[plotting]["median"],
-        "-o",
-        label=f"""Median {plotting}.""",
-    )
+fig.tight_layout()
+fig.savefig("TTS.svg", transparent=True, bbox_inches="tight", pad_inches=0.1)
 
-    axes[mat].fill_between(
-        psize,
-        data[plotting]["min"],
-        data[plotting]["max"],
-        alpha=0.2,
-        label=f"Min/Max {plotting}.",
-    )
-
-    axes[mat].plot(
-        psize,
-        data[plotting]["median"][0] / psize if plotting == "TTS" else psize,
-        "-",
-        label=f"Ideal {plotting}.",
-    )
-
-labels = [psize[0]]
-for label in psize:
-    if label >= labels[-1] * 2:
-        labels.append(label)
-
-del labels[-1]
-labels.append(psize[-1])
-print(labels)
-
-for name, ax in axes.items():
-    ax.set_xscale("log", base=2)
-    ax.set_yscale("log")
-    ax.set_ylabel(f"""{plotting} [{"s" if plotting == "TTS" else "-"}]""")
-    ax.set_xticks(ticks=labels, labels=[str(label) for label in labels])
-
-axes["cohesive"].set_title("b) With cohesive insertion")
-axes["elastic"].set_title("a) Without cohesive insertion")
-
-plt.xlabel("Nb Cores [-]")
-plt.legend(bbox_to_anchor=(1.04, 2), loc="center left", borderaxespad=0)
-
-plt.subplots_adjust(right=0.7)
-plt.savefig(f"{plotting}.svg", bbox_inches="tight")
 plt.show()
