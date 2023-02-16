fcc8ce46a41615ae1517a5276a36472baeb88b24
Emil Gallyamov <emil.gallyamov@epfl.ch>
2023-02-16 10:50:54 +0100

    Correcting coefficients in heat transfer model

--- src/model/common/integration_scheme/generalized_trapezoidal.cc
+++ src/model/common/integration_scheme/generalized_trapezoidal.cc
@@ -182,11 +182,11 @@ void GeneralizedTrapezoidal::assembleJacobian(const SolutionType & type,
   J.copyProfile(K);
   // J.zero();
 
-  Real c = this->getTemperatureRateCoefficient(type, delta_t);
   Real e = this->getTemperatureCoefficient(type, delta_t);
+  Real d = this->getTemperatureRateCoefficient(type, delta_t);
 
-  J.add(M, e);
-  J.add(K, c);
+  J.add(M, d);
+  J.add(K, e);
 
   m_release = M.getRelease();
   k_release = K.getRelease();
