5060c953e660b593b1a8b618710917a5251a86ec
Emil Gallyamov <emil.gallyamov@epfl.ch>
2023-12-27 17:49:03 +0100

    Registering synchronizers for normals and opening on cohesives

--- src/model/solid_mechanics/solid_mechanics_model_cohesive/solid_mechanics_model_cohesive.cc
+++ src/model/solid_mechanics/solid_mechanics_model_cohesive/solid_mechanics_model_cohesive.cc
@@ -160,6 +160,12 @@ SolidMechanicsModelCohesive::SolidMechanicsModelCohesive(
                                SynchronizationTag::_smm_stress);
     this->registerSynchronizer(*cohesive_synchronizer,
                                SynchronizationTag::_smm_boundary);
+    this->registerSynchronizer(*cohesive_synchronizer,
+                               SynchronizationTag::_smmc_normal);
+    this->registerSynchronizer(*cohesive_synchronizer,
+                               SynchronizationTag::_smmc_opening);
+    this->registerSynchronizer(*cohesive_synchronizer,
+                               SynchronizationTag::_smmc_damage);
   }
 
   this->inserter = std::make_unique<CohesiveElementInserter>(
